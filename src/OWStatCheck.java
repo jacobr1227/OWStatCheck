d749f7163dacf7b90137a0e2b2385a64ac0a7826
jrogers15 <jrogers15@twu.edu>
2021-04-30 11:43:40 -0500

    FINALLY DONEgit add *!

--- src/OWStatCheck.java
+++ src/OWStatCheck.java
@@ -3,9 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.net.*;
+import java.io.*;
 class OWStatCheck implements ActionListener {
     final JFrame stWin = new JFrame("OW Stat Checker");
     JPanel panel = new JPanel(new BorderLayout());
+    JPanel menuP = new JPanel(new BorderLayout());
     JMenuBar mb = new JMenuBar();
     JMenu mS = new JMenu("System");
     JMenu mH = new JMenu("Help");
@@ -15,26 +17,42 @@ class OWStatCheck implements ActionListener {
     JMenuItem hH = new JMenuItem("How to...");
     JMenuItem hA = new JMenuItem("About");
     JTextArea ta = new JTextArea("Stats:");
-    String[] heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lucio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjorn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
+    JScrollPane jsp = new JScrollPane(ta);
+    String[] heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Bastion","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lúcio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjörn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
     JComboBox<String> jcb = new JComboBox<String>(heroList);
+    ComboBoxModel<String> model = jcb.getModel();
+    JButton update = new JButton("Get Hero Info");
     JLabel jl = new JLabel("Mode: Quick Play");
+    JLabel spacer = new JLabel("    ");
     JMenuItem cqp = new JMenuItem("Switch Stats");
+    JFileChooser jfc = new JFileChooser();
     String bt,rgn,plat;
-    ArrayList<String> dataList = new ArrayList<String>();
+    ArrayList<String> vals = new ArrayList<String>();
+    String[] SR;
+    String[] typ = {"Tank", "DPS", "Support"};
+    int ct;
     boolean comp = false;
-    public OWStatCheck() {
+    public OWStatCheck() { //TODO: fix comp overextension from Zen, fix comp name association / Add comments
         stWin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setPreferredSize(new Dimension(400,750));
+        
+        panel.add(jsp);
         ta.setEditable(false);
         ta.setOpaque(false);
-
+        jfc.setDialogTitle("Specify a file to save:");
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
         mb.add(mS);
         mb.add(mH);
+        mb.add(spacer);
         mS.add(sN);
         mS.add(sS);
         mS.add(sE);
         mH.add(hH);
         mH.add(hA);
         mS.add(cqp);
+        jcb.setPreferredSize(new Dimension(100,50));
 
         cqp.addActionListener(this);
         sN.addActionListener(this);
@@ -43,12 +61,14 @@ class OWStatCheck implements ActionListener {
         hH.addActionListener(this);
         hA.addActionListener(this);
         mH.addActionListener(this);
+        jcb.addActionListener(this);
 
-        stWin.setContentPane(panel);
-        stWin.getContentPane().add(BorderLayout.NORTH, mb);
-        stWin.getContentPane().add(BorderLayout.SOUTH, jl);
-        stWin.getContentPane().add(BorderLayout.CENTER, ta);
-        stWin.getContentPane().add(BorderLayout.CENTER, jcb);
+        stWin.add(BorderLayout.NORTH,menuP);
+        stWin.add(BorderLayout.CENTER,panel);
+        menuP.add(BorderLayout.NORTH, mb);
+        panel.add(BorderLayout.NORTH, jl);
+        panel.add(BorderLayout.SOUTH, jcb);
+        panel.add(BorderLayout.CENTER, jsp);
         stWin.setVisible(true);
         stWin.pack();
     }
@@ -67,40 +87,127 @@ class OWStatCheck implements ActionListener {
             while(plat==null) {}
             try {
                 URL url=new URL(urlGen(bt,rgn,plat));
-                infoGrabber ig = new infoGrabber(url, comp);
+                infoGrabber ig = new infoGrabber(url);
                 ig.execute();
-                jcb.setSelectedIndex(0);
-                ta.setText(ig.values.get(1));
-                /*String[] SR = ig.getComp();
-                String[] typ = {"Tank", "DPS", "Support"};
-                for(int i=0;i<SR.length;i++) {
+                SR = ig.getComp();
+                vals = ig.getReturns();
+                comp = false;
+                for(int i=0;i<3;i++) {
                     ta.append(typ[i]+" SR: "+SR[i]+"\n");
                 }
-                for(int i=0;i<ig.values.size();i++) {
-                    if(i%60==0||i==0) { //find names to properly stylize
-                        ta.append(ig.values.get(i)+"\n");
-                    }
-                    else { //clean html later
-                        
-                    }
-                }*/
+                for(int i=0;i<60;i++) {
+                    ta.append(vals.get(i)+"\n");
+                }
+                jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
             } catch(Exception malformedUrlException) {malformedUrlException.printStackTrace();}
         }
         if(e.getSource()==jcb) {
-            
+            ta.setText("");
+            if(!comp) {
+            for(int i=0;i<heroList.length;i++) {
+                if(heroList[i]==jcb.getSelectedItem()) {
+                    for(int a=0;a<vals.size();a++) {
+                        if(vals.get(a)==heroList[i]) {
+                            for(int z=0;z<3;z++) {
+                                ta.append(typ[z]+" SR: "+SR[z]+"\n");
+                            }
+                            if(vals.get(a).equals(heroList[32])) {
+                                while(vals.get(a)!="Competitive") {
+                                    if(!vals.get(a).equals("All Heroes")) {
+                                        ta.append(vals.get(a)+"\n");
+                                    }
+                                    a++;
+                                }
+                                break;
+                            }
+                            while(vals.get(a)!=heroList[i+1]&&vals.get(a)!="Competitive"&&a<vals.size()) {
+                                ta.append(vals.get(a)+"\n");
+                                System.out.println(i +" " + a + " " + vals.get(a));
+                                a++;
+                            }
+                            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+                            break;
+                        }
+                    }
+                }
+            }
+            }
+            if(comp) {
+            for(int i=0;i<heroList.length;i++) {
+                System.out.println(i+"i");
+                if(heroList[i]==jcb.getSelectedItem()) {
+                    for(int a=0;a<vals.size();a++) {
+                        if(vals.get(a)=="Competitive") {
+                            ta.append(vals.get(a)+"\n");
+                            for(int r=a+1;r<vals.size();r++) {
+                                if(vals.get(r)==heroList[i]) {
+                                    for(int z=0;z<3;z++) {
+                                        ta.append(typ[z]+" SR: "+SR[z]+"\n");
+                                    }
+                                    if(vals.get(r).equals(heroList[32])) {
+                                        while(r<vals.size()) {
+                                            ta.append(vals.get(r)+"\n");
+                                            r++;
+                                        }
+                                        break;
+                                    }
+                                    while(vals.get(r)!=heroList[i+1]&&r<vals.size()) {
+                                        ta.append(vals.get(r)+"\n");
+                                        System.out.println(i + " " + a + " " + r + " " + vals.get(r));
+                                        r++;
+                                    }
+                                    jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+                                    break;
+                                }
+                                if(i==0) {
+                                    for(int z=0;z<3;z++) {
+                                        ta.append(typ[z]+" SR: " + SR[z]+"\n");
+                                    }
+                                    while(vals.get(r)!=heroList[1]&&r<vals.size()) {
+                                        ta.append(vals.get(r)+"\n");
+                                        System.out.println(i + " " + a + " " + r + " " + vals.get(r));
+                                        r++;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            }
         }
         if(e.getSource()==cqp) {
-            if(!comp)
+            if(plat ==null) {
+                JOptionPane.showMessageDialog(panel, "Please open a profile first!");
+                
+            }
+            else {
+            if(!comp) {
                 jl.setText("Mode: Competitive");
-            if(comp)
-                jl.setText("Mode: Quick Play");
-            comp=!comp;
-        }
-        if(e.getSource()==sS) { //potentially add a data export feature?
 
+            }
+            if(comp) {
+                jl.setText("Mode: Quick Play");
+            }
+            comp = !comp;
+            jcb.setSelectedIndex(0);
+            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+            }
+        }
+        if(e.getSource()==sS) { //let's go, data exporting
+            int saveSelect = jfc.showSaveDialog(stWin);
+            if(saveSelect == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File toSave = jfc.getSelectedFile();
+                    FileWriter fw = new FileWriter(toSave);
+                    fw.write(ta.getText());
+                    fw.close();
+                }catch(Exception IOException){IOException.printStackTrace();}
+            }
         }
         if(e.getSource()==hH) {
-            JOptionPane.showMessageDialog(stWin,"To open a profile, select [ System>Open new Profile ] and complete the dialogs.\nTo exit the application, please click the red X, or use [ System>Exit ]\nExporting data is currently not implemented, but may be in the future.");
+            JOptionPane.showMessageDialog(stWin,"To open a profile, select [ System>Open new Profile ] and complete the dialogs.\nTo exit the application, please click the red X, or use [ System>Exit ]\nIf you see \"null\" in the SRs, you may have mistyped the name. Try reopening your profile.\nIf you see the wrong stats on heroes, or Zenyatta is broken, you haven't played a hero yet. This WILL cause the app to break.");
         }
         if(e.getSource()==hA) {
             JOptionPane.showMessageDialog(stWin, "This application was made by Jacob Rogers. I do hope you enjoy or find utility in it.\nIf you don't play Overwatch, and would like an example profile, use mine: Skjell#1659");
--- src/infoGrabber.java
+++ src/infoGrabber.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.util.regex.*;
 import java.io.*;
-import java.lang.*;
 import java.net.*;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
@@ -10,124 +9,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 public class infoGrabber {
-    Map<String,String> heroMap = new HashMap<String,String>();
-    Map<String,String> statMap = new HashMap<String,String>();
     String [] heroList;
     {       //listing all of our hero identifiers for HTML parsing
-        heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lucio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjorn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
-        heroMap.put("0x02E00000FFFFFFFF","ALL HEROES");
-        heroMap.put("0X02E000000000013B","Ana");
-        heroMap.put("0X02E0000000000200","Ashe");
-        heroMap.put("0X02E0000000000221","Baptiste");
-        heroMap.put("0X02E0000000000015","Brigitte");
-        heroMap.put("0X02E0000000000195","D.va");
-        heroMap.put("0X02E000000000007A","Doomfist");
-        heroMap.put("0X02E000000000012F","Echo");
-        heroMap.put("0X02E0000000000206","Genji");
-        heroMap.put("0X02E0000000000029","Hanzo");
-        heroMap.put("0X02E0000000000005","Junkrat");
-        heroMap.put("0X02E0000000000065","Lucio");
-        heroMap.put("0X02E0000000000079","McCree");
-        heroMap.put("0X02E0000000000042","Mei");
-        heroMap.put("0X02E00000000000DD","Mercy");
-        heroMap.put("0X02E0000000000004","Moira");
-        heroMap.put("0X02E00000000001A2","Orisa");
-        heroMap.put("0X02E000000000013E","Pharah");
-        heroMap.put("0X02E0000000000008","Reaper");
-        heroMap.put("0X02E0000000000002","Reinhardt");
-        heroMap.put("0X02E0000000000007","Roadhog");
-        heroMap.put("0X02E0000000000040","Sigma");
-        heroMap.put("0X02E000000000023B","Soldier: 76");
-        heroMap.put("0X02E000000000006E","Sombra");
-        heroMap.put("0X02E000000000012E","Symmetra");
-        heroMap.put("0X02E0000000000006","Torbjorn");
-        heroMap.put("0X02E0000000000003","Tracer");
-        heroMap.put("0X02E000000000000A","Widowmaker");
-        heroMap.put("0X02E0000000000009","Winston");
-        heroMap.put("0X02E00000000001CA","Wrecking Ball");
-        heroMap.put("0X02E0000000000068","Zarya");
-        heroMap.put("0X02E0000000000020","Zenyatta");
-                //listing all of our stat identifiers for HTML parsing
-        //BEST tab (19 values / 38 pieces)
-        statMap.put("All Damage - Most in Game","0x08600000000001E3");
-        statMap.put("Barrier Damage - Most in Game","0x086000000000052A");
-        statMap.put("Defensive Assists - Most in Game","0x0860000000000319");
-        statMap.put("Eliminations - Most in Game","0x08600000000001B9");
-        statMap.put("Environmental Kills - Most in Game","0x0860000000000495");
-        statMap.put("Final Blows - Most in Game","0x08600000000001E2");
-        statMap.put("Healing Done - Most in Game","0x08600000000001E4");
-        statMap.put("Hero Damage Done - Most in Game","0x08600000000004BA");
-        statMap.put("Kill Streak - Best","0x0860000000000497");
-        statMap.put("Melee Final Blows - Most in Game","0x08600000000003ED");
-        statMap.put("Multikill - Best","0x0860000000000348");
-        statMap.put("Objective Kills - Most in Game","0x0860000000000322");
-        statMap.put("Objective Time - Most in Game","0x0860000000000323");
-        statMap.put("Offensive Assists - Most in Game","0x086000000000031A");
-        statMap.put("Recon Assists - Most in Game","0x086000000000034B");
-        statMap.put("Solo Kills - Most in Game","0x086000000000036B");
-        statMap.put("Teleporter Pad Destroyed - Most in Game","0x0860000000000496");
-        statMap.put("Time Spent on Fire - Most in Game","0x08600000000003CB");
-        statMap.put("Turrets Destroyed - Most in Game","0x0860000000000494");
-        //ASSISTS tab (4 values / 8 pieces)
-        statMap.put("Defensive Assists","0x0860000000000317");
-        statMap.put("Healing Done","0x08600000000001D1");
-        statMap.put("Offensive Assists","0x0860000000000318");
-        statMap.put("Recon Assists","0x086000000000034A");
-        //COMBAT tab (14 values / 28 pieces)
-        statMap.put("All Damage Done","0x08600000000000C9");
-        statMap.put("Barrier Damage Done","0x0860000000000516");
-        statMap.put("Damage Done","0x0860000000000621");
-        statMap.put("Deaths","0x0860000000000029");
-        statMap.put("Eliminations","0x0860000000000025");
-        statMap.put("Environmental Kills","0x0860000000000363");
-        statMap.put("Final Blows","0x086000000000002C");
-        statMap.put("Hero Damage done","0x08600000000004B8");
-        statMap.put("Melee Final Blows","0x0860000000000381");
-        statMap.put("Multikills","0x0860000000000347");
-        statMap.put("Objective Kills","0x0860000000000326");
-        statMap.put("Objective Time","0x0860000000000327");
-        statMap.put("Solo Kills","0x086000000000002E");
-        statMap.put("Time Spent on Fire","0x08600000000003CD");
-        //MATCH AWARDS tab (5 values / 10 pieces)
-        statMap.put("Cards","0x0860000000000376");
-        statMap.put("Medals - Gold","0x0860000000000372");
-        statMap.put("Medals","0x0860000000000374");
-        statMap.put("Medals - Bronze","0x0860000000000370");
-        statMap.put("Medals - Silver","0x0860000000000371");
-        //MISCELLANEOUS tab (2 values / 4 pieces)
-        statMap.put("Teleporter Pads Destroyed","0x0860000000000329");
-        statMap.put("Turrets Destroyed","0x0860000000000491");
-        //GAME tab (4 values / 8 pieces)
-        statMap.put("Games Lost","0x086000000000042E");
-        statMap.put("Games Played","0x0860000000000385");
-        statMap.put("Games Won","0x08600000000003F5");
-        statMap.put("Time Played","0x0860000000000026");
-        //AVERAGE tab (11 values / 22 pieces)
-        statMap.put("All Damage Done - Avg per 10 min","0x0860000000000386");
-        statMap.put("Barrier Damage Done - Avg per 10 min","0x0860000000000519");
-        statMap.put("Deaths - Avg per 10 min","0x08600000000004C3");
-        statMap.put("Eliminations - Avg per 10 min","0x08600000000004B0");
-        statMap.put("Final Blows - Avg per 10 min","0x08600000000004B1");
-        statMap.put("Healing Done - Avg per 10 min","0x08600000000004B2");
-        statMap.put("Hero Damage Done - Avg per 10 min","0x08600000000004C1");
-        statMap.put("Objective Kills - Avg per 10 min","0x08600000000004B3");
-        statMap.put("Objective Time - Avg per 10 min","0x08600000000004B4");
-        statMap.put("Solo Kills - Avg per 10 min","0x08600000000004B5");
-        statMap.put("Time Spent on Fire - Avg per 10 min","0x08600000000004B6");
-    } //both maps are now redundant, but rather than delete them I kept it here as a reference sheet if I needed it
+        heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Bastion","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lúcio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjörn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
+    }
     public ArrayList<String> values = new ArrayList<String>();
-    String[] compRates;
+    public ArrayList<String> heroConfirm = new ArrayList<String>();
+    public ArrayList<String> compConfirm = new ArrayList<String>();
+    public String[] compRates;
     URL url;
-    boolean comp;
-    public infoGrabber(URL newrl, boolean ctf) {
+    public infoGrabber(URL newrl) {
         url = newrl;
-        comp = ctf;
     }
     public void execute() throws IOException {
         //use URL info generated in regular class to find a player page
         if(new File("idump.txt").delete()){System.out.println("Cleared.");} //a safe way to make sure this file gets overwritten rather than appended
         values.clear();
-        values.add("All Heroes");
         ReadableByteChannel rbc = Channels.newChannel(url.openStream());
         FileOutputStream fos = new FileOutputStream("idump.txt");
         FileChannel fc = fos.getChannel();
@@ -135,56 +32,134 @@ public class infoGrabber {
         fos.close();
         String line;
         Stream<String> lines = Files.lines(Paths.get("idump.txt"));
-        line = lines.skip(1523).findFirst().get();
-        System.out.println(line);
+        line = lines.skip(1522).findFirst().get();
         lines.close();
         String nex = "";
-        if(this.comp){
-            System.out.println("Progress");
-            nex = line;
-            Pattern pattern = Pattern.compile("(competitive-rank-level\">\\d{3,4})");
-            Matcher matcher = pattern.matcher(nex);
-            String[] srs = new String[3];
-            int x=0;
-            while(matcher.find() && x<3) {
-                srs[x] = matcher.group(0);
-                x++;
+
+        nex = line;
+        Pattern pattern = Pattern.compile("(competitive-rank-level\">\\d{3,4})");
+        Matcher matcher = pattern.matcher(nex);
+        String[] srs = new String[3];
+        int x=0;
+        while(matcher.find() && x<3) {
+            srs[x] = matcher.group(0);
+            srs[x] = srs[x].replaceAll("competitive-rank-level\">", "");
+            x++;
+        }
+        pattern = Pattern.compile("(0x02E\\d*\\w*)\"\\soption-id=\"([a-zA-Z]*\\s?.?\\s?[a-zA-Z0-9]*)");
+        matcher = pattern.matcher(nex);
+        int cycle = 0;
+        int loca = 0;
+        while(matcher.find()) {
+            if(matcher.group(0).contains("ALL HEROES")) {
+                cycle++;
+                loca=0;
             }
-            compRates = new String[] {srs[0],srs[1],srs[2]}; //[0] contains tank, [1] contains DPS, [2] contains support
-            nex=line;
-            pattern = Pattern.compile("([a-zA-Z]*(-?\\s[a-zA-z]*)*</td><[^>]*>(\\d*:?\\d*))");
-            matcher = pattern.matcher(nex);
-            String found = "";
-            //goal is to replace anything that is NOT (number/word number/word...number/word</td><...>number/decimal/time</td></tr>) with nothing
-            while(matcher.find()) {
-                found = matcher.group(0);
+            if(cycle==1) {
+                heroConfirm.add(matcher.group(0).replaceAll("(0x02E\\d*\\w*)\"\\soption-id=\"", ""));
+                if(heroConfirm.get(loca).endsWith("\"")) {
+                    heroConfirm.set(loca, heroConfirm.get(loca).substring(0,heroConfirm.get(loca).length()-1));
+                }
             }
-            nex.replaceAll("</td><td class=\"DataTable-tableColumn\">", ": ");
-            String[] stList = nex.split("</td></tr>"); //then we clean it up based on the remaining tags
-            int z=0;
-            for(int i=0;i<stList.length;i++) {
-                System.out.println("Loop!");
-                if(stList[i].contains("All Damage Done")) {
+            if(cycle==2) {
+                compConfirm.add(matcher.group(0).replaceAll("(0x02E\\d*\\w*)\"\\soption-id=\"", ""));
+                if(compConfirm.get(loca).endsWith("\"")) {
+                    compConfirm.set(loca, compConfirm.get(loca).substring(0,compConfirm.get(loca).length()-1));
+                }
+            }
+            loca++;
+        }
+        compRates = new String[] {srs[0],srs[1],srs[2]}; //[0] contains tank, [1] contains DPS, [2] contains support
+        pattern = Pattern.compile("([a-zA-Z]*(-*\\s*\\d*[a-zA-z]*)*</td><[^>]*>(\\d*:?\\.?%?\\d*:?\\d*))");
+        matcher = pattern.matcher(nex);
+        String found = "";
+        int z=0;
+        boolean firstDamage = false; // a boolean that fixes Ana's stats appearing under All Heroes
+        boolean ahnm = true; // a boolean that fixes the order of name addition and generation
+        boolean c = false;
+        boolean luciohs = false;
+        boolean onemore = false;
+        //goal is to replace anything that is NOT (number/word number/word...number/word</td><...>number/decimal/time</td></tr>) with nothing
+        while(matcher.find()) {
+            found = matcher.group(0);
+            found = found.replace("</td><td class=\"DataTable-tableColumn\">", ": ");
+            found = found.replace("</td></tr>", "");
+            if(found.startsWith("rn Kills")) {
+                found =found.replace("rn Kills","Torbjörn Kills");
+            }
+            if(found.startsWith("d:") || found.startsWith("d -")) {
+                found = found.replace("d", "Enemies EMP'd");
+            }
+            if(found.contains("All Damage Done - Most in Game")) {
+                if(ahnm) { 
+                            ahnm = false;
+                            values.add(heroList[z]);
+                            z++;
+                }
+                if(c) {
+                    if(onemore) {
+                        values.add("Competitive");
+                        onemore = false;
+                    }
+                    else {
+                        onemore = true;
+                    }
+                }
+                else if(values.size()>0) {
+                    if(values.get(values.size()-1).contains("Time Spent on Fire") || values.get(values.size()-1).contains("Competitive")) {
+                        values.add(heroList[z]);
+                        z++;
+                        luciohs = true;
+                    }
+                }
+            }
+            if(found.contains("All Damage Done:")&& firstDamage&&!c &&!luciohs) {
+                while(!heroConfirm.contains(heroList[z]) && z<34) {
                     values.add(heroList[z]);
-                    System.out.println(heroList[z]);
                     z++;
                 }
-                if(z==31) {values.add("Competitive");z=0;}
-                values.add(stList[i]); //and add it all into our super array
-                System.out.println(stList[i]);
+                if(z<34) {
+                values.add(heroList[z]);
+                z++;
+                onemore=false;
+                }
+            }
+            if(found.contains("All Damage Done:") && firstDamage&&c&&!luciohs) {
+                while(!compConfirm.contains(heroList[z]) && z<32) {
+                    values.add(heroList[z]);
+                    z++;
+                }
+                if(z<33) {
+                values.add(heroList[z]);
+                onemore=false;
+                z++;
+                }
+            }
+            if(found.contains("All Damage Done:") && !firstDamage) {
+                firstDamage = true;
+            }
+            if(luciohs && found.contains("All Damage Done:")) {
+                luciohs = false;
+            }
+            if(z>=33){z=0;firstDamage=false;c=true;ahnm=true;onemore=false;}
+            if(found!="" && found!= " ") {
+                values.add(found);
             }
         }
-        
+        System.out.println("Done.");
     }
+        
 
-    public ArrayList<String> getReturns() { //TODO: implement error catch for nonexistent profiles
-        System.out.println(values);
+    public ArrayList<String> getReturns() {
         return values;
     }
     public String[] getComp() {
         return compRates;
     }
-    public String[] getHeroes() {
-        return heroList;
+    public ArrayList<String> getqpHeroes() {
+        return heroConfirm;
+    }
+    public ArrayList<String> getcHeroes() {
+        return compConfirm;
     }
 }
