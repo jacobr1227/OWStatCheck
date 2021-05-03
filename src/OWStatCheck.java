9eaadac38f4b58580e29a3743f3016d3ae0676e2
jrogers15 <jrogers15@twu.edu>
2021-05-02 21:12:39 -0500

    finalized, commented

--- src/OWStatCheck.java
+++ src/OWStatCheck.java
@@ -16,25 +16,24 @@ class OWStatCheck implements ActionListener {
     JMenuItem sE = new JMenuItem("Exit");
     JMenuItem hH = new JMenuItem("How to...");
     JMenuItem hA = new JMenuItem("About");
-    JTextArea ta = new JTextArea("Stats:");
+    JTextArea ta = new JTextArea("Stats:");     //love the jmenu
     JScrollPane jsp = new JScrollPane(ta);
     String[] heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Bastion","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lúcio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjörn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
-    JComboBox<String> jcb = new JComboBox<String>(heroList);
-    ComboBoxModel<String> model = jcb.getModel();
+    JComboBox<String> jcb = new JComboBox<String>(heroList);     
     JButton update = new JButton("Get Hero Info");
     JLabel jl = new JLabel("Mode: Quick Play");
     JLabel spacer = new JLabel("    ");
     JMenuItem cqp = new JMenuItem("Switch Stats");
-    JFileChooser jfc = new JFileChooser();
+    JFileChooser jfc = new JFileChooser();  //at some point my nomenclature became shortening the constructor name
     String bt,rgn,plat;
     ArrayList<String> vals = new ArrayList<String>();
     String[] SR;
-    String[] typ = {"Tank", "DPS", "Support"};
+    String[] typ = {"Tank", "DPS", "Support"};      //it seems like a lot of setup, right?
     int ct;
     boolean comp = false;
-    public OWStatCheck() { //TODO: fix comp overextension from Zen, fix comp name association / Add comments
+    public OWStatCheck() {
         stWin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel.setPreferredSize(new Dimension(400,750));
+        panel.setPreferredSize(new Dimension(400,750)); //look at that, we have a size modifier, wow
         
         panel.add(jsp);
         ta.setEditable(false);
@@ -42,11 +41,11 @@ class OWStatCheck implements ActionListener {
         jfc.setDialogTitle("Specify a file to save:");
         jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+        jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());   //this actually doesn't work
         mb.add(mS);
         mb.add(mH);
         mb.add(spacer);
-        mS.add(sN);
+        mS.add(sN);     //love the meny setup
         mS.add(sS);
         mS.add(sE);
         mH.add(hH);
@@ -61,9 +60,9 @@ class OWStatCheck implements ActionListener {
         hH.addActionListener(this);
         hA.addActionListener(this);
         mH.addActionListener(this);
-        jcb.addActionListener(this);
+        jcb.addActionListener(this);        //I forgot this listener for a while, that one hurt to find
 
-        stWin.add(BorderLayout.NORTH,menuP);
+        stWin.add(BorderLayout.NORTH,menuP);        //we use two panels here so that I can add two objects to NORTH without overlapping
         stWin.add(BorderLayout.CENTER,panel);
         menuP.add(BorderLayout.NORTH, mb);
         panel.add(BorderLayout.NORTH, jl);
@@ -77,74 +76,73 @@ class OWStatCheck implements ActionListener {
         if(e.getSource()==sE) {
             stWin.dispose();
         }
-        if(e.getSource()==sN) {
+        if(e.getSource()==sN) {     //menu: Open Profile
             ta.setText("");
             bt = (String)JOptionPane.showInputDialog(stWin, "Please enter your battletag:", "Example#1234");
             while(bt==null) {}
             rgn = (String)JOptionPane.showInputDialog(stWin, "Please enter your game region as \" **-** \" (language-region):", "en-us");
             while(rgn==null) {}
             plat = (String)JOptionPane.showInputDialog(stWin, "Please enter your platform:","pc");
-            while(plat==null) {}
+            while(plat==null) {}    //horrible design mistake just to make sure nothing else breaks. Please never hit cancel...
             try {
-                URL url=new URL(urlGen(bt,rgn,plat));
-                infoGrabber ig = new infoGrabber(url);
-                ig.execute();
-                SR = ig.getComp();
+                URL url=new URL(urlGen(bt,rgn,plat)); //create a url to use over on the other side
+                infoGrabber ig = new infoGrabber(url);  //and make our constructor
+                ig.execute();   //and carry it out.
+                SR = ig.getComp();  //utilize out returns
                 vals = ig.getReturns();
-                comp = false;
+                comp = false;      //defaulting the UI so that no errors occur and there is no need for dual functionality
                 for(int i=0;i<3;i++) {
-                    ta.append(typ[i]+" SR: "+SR[i]+"\n");
+                    ta.append(typ[i]+" SR: "+SR[i]+"\n");   //add in the Skill ratings
                 }
                 for(int i=0;i<60;i++) {
-                    ta.append(vals.get(i)+"\n");
+                    ta.append(vals.get(i)+"\n");    //add in all of our first 60 data pieces, which is exactly that of All Heroes (QP)
                 }
-                jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
-            } catch(Exception malformedUrlException) {malformedUrlException.printStackTrace();}
+                jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());   //this still does nothing
+            } catch(Exception malformedUrlException) {malformedUrlException.printStackTrace();} //and of course it won't work without this
         }
-        if(e.getSource()==jcb) {
-            ta.setText("");
-            if(!comp) {
+        if(e.getSource()==jcb) {    //welcome to loop hell
+            ta.setText("");         //we have no cookies
+            if(!comp) {             //only dyslexia
             for(int i=0;i<heroList.length;i++) {
                 if(heroList[i]==jcb.getSelectedItem()) {
                     for(int a=0;a<vals.size();a++) {
                         if(vals.get(a)==heroList[i]) {
                             for(int z=0;z<3;z++) {
-                                ta.append(typ[z]+" SR: "+SR[z]+"\n");
+                                ta.append(typ[z]+" SR: "+SR[z]+"\n");   //adding in our SR before every character because why not
                             }
                             if(vals.get(a).equals(heroList[32])) {
-                                while(vals.get(a)!="Competitive") {
+                                while(vals.get(a)!="Competitive") { //run the loop until we hit this point in the data
                                     if(!vals.get(a).equals("All Heroes")) {
-                                        ta.append(vals.get(a)+"\n");
+                                        ta.append(vals.get(a)+"\n");        //this is Zenyatta's special place, this allows the below function to be easier to read and write
                                     }
                                     a++;
                                 }
                                 break;
                             }
-                            while(vals.get(a)!=heroList[i+1]&&vals.get(a)!="Competitive"&&a<vals.size()) {
+                            while(vals.get(a)!=heroList[i+1]&&vals.get(a)!="Competitive"&&a<vals.size()) { //this works for everyone from All Heroes to Zarya. And then the array freaks out at Zenyatta.
                                 ta.append(vals.get(a)+"\n");
-                                System.out.println(i +" " + a + " " + vals.get(a));
-                                a++;
-                            }
-                            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+                                a++;    //now you may be wondering why I have a for loop over a and then a while loop instead.
+                            }           //"It just works" - Todd Howard
+                            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum()); //yeah it still doesn't work
                             break;
                         }
                     }
                 }
             }
             }
-            if(comp) {
+            if(comp) {  //okay now everything we just did? Do it again, but with one more conditional
             for(int i=0;i<heroList.length;i++) {
                 System.out.println(i+"i");
                 if(heroList[i]==jcb.getSelectedItem()) {
                     for(int a=0;a<vals.size();a++) {
-                        if(vals.get(a)=="Competitive") {
+                        if(vals.get(a)=="Competitive") {    //this time we're gonna skip to this value
                             ta.append(vals.get(a)+"\n");
                             for(int r=a+1;r<vals.size();r++) {
                                 if(vals.get(r)==heroList[i]) {
                                     for(int z=0;z<3;z++) {
                                         ta.append(typ[z]+" SR: "+SR[z]+"\n");
                                     }
-                                    if(vals.get(r).equals(heroList[32])) {
+                                    if(vals.get(r).equals(heroList[32])) {  //and print everything much the same
                                         while(r<vals.size()) {
                                             ta.append(vals.get(r)+"\n");
                                             r++;
@@ -153,7 +151,6 @@ class OWStatCheck implements ActionListener {
                                     }
                                     while(vals.get(r)!=heroList[i+1]&&r<vals.size()) {
                                         ta.append(vals.get(r)+"\n");
-                                        System.out.println(i + " " + a + " " + r + " " + vals.get(r));
                                         r++;
                                     }
                                     jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
@@ -163,9 +160,8 @@ class OWStatCheck implements ActionListener {
                                     for(int z=0;z<3;z++) {
                                         ta.append(typ[z]+" SR: " + SR[z]+"\n");
                                     }
-                                    while(vals.get(r)!=heroList[1]&&r<vals.size()) {
+                                    while(vals.get(r)!=heroList[1]&&r<vals.size()) {    //we also have a compatibility layer for All Heroes (C)
                                         ta.append(vals.get(r)+"\n");
-                                        System.out.println(i + " " + a + " " + r + " " + vals.get(r));
                                         r++;
                                     }
                                     break;
@@ -179,24 +175,22 @@ class OWStatCheck implements ActionListener {
         }
         if(e.getSource()==cqp) {
             if(plat ==null) {
-                JOptionPane.showMessageDialog(panel, "Please open a profile first!");
-                
+                JOptionPane.showMessageDialog(panel, "Please open a profile first!"); //only one error message here.
             }
             else {
             if(!comp) {
                 jl.setText("Mode: Competitive");
-
             }
             if(comp) {
                 jl.setText("Mode: Quick Play");
             }
             comp = !comp;
             jcb.setSelectedIndex(0);
-            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());
+            jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMinimum());   //lo and behold it stil doesn't do what it's supposed to.
             }
         }
         if(e.getSource()==sS) { //let's go, data exporting
-            int saveSelect = jfc.showSaveDialog(stWin);
+            int saveSelect = jfc.showSaveDialog(stWin); //I almost excluded this but it really was a lot easier than i thought
             if(saveSelect == JFileChooser.APPROVE_OPTION) {
                 try {
                     File toSave = jfc.getSelectedFile();
@@ -204,22 +198,22 @@ class OWStatCheck implements ActionListener {
                     fw.write(ta.getText());
                     fw.close();
                 }catch(Exception IOException){IOException.printStackTrace();}
-            }
+            }   //I maybe should add an option for printing all of your data, not just the open page, but whatever.
         }
-        if(e.getSource()==hH) {
+        if(e.getSource()==hH) { //help menu
             JOptionPane.showMessageDialog(stWin,"To open a profile, select [ System>Open new Profile ] and complete the dialogs.\nTo exit the application, please click the red X, or use [ System>Exit ]\nIf you see \"null\" in the SRs, you may have mistyped the name. Try reopening your profile.\nIf you see the wrong stats on heroes, or Zenyatta is broken, you haven't played a hero yet. This WILL cause the app to break.");
         }
-        if(e.getSource()==hA) {
+        if(e.getSource()==hA) { //about menu
             JOptionPane.showMessageDialog(stWin, "This application was made by Jacob Rogers. I do hope you enjoy or find utility in it.\nIf you don't play Overwatch, and would like an example profile, use mine: Skjell#1659");
         }
     }
 
-    public String urlGen(String battletag, String region, String platform) {
+    public String urlGen(String battletag, String region, String platform) {    //wow, one whole function.
         battletag = battletag.replace("#","-");
         return "https://playoverwatch.com/" + region + "/career/" + platform + "/" + battletag + "/";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //shortest main in my life
             new OWStatCheck();
     }
 }
--- src/infoGrabber.java
+++ src/infoGrabber.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.regex.*;
 import java.io.*;
 import java.net.*;
-import java.nio.channels.Channels;
+import java.nio.channels.Channels; //importing the nio libraries for easy file acquisition and intake.
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
@@ -10,47 +10,48 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 public class infoGrabber {
     String [] heroList;
-    {       //listing all of our hero identifiers for HTML parsing
+    {       //listing all of our hero identifiers for working thorough the HTML to get data separated
         heroList=new String[] {"All Heroes", "Ana","Ashe","Baptiste","Bastion","Brigitte","D.Va","Doomfist","Echo","Genji","Hanzo","Junkrat","Lúcio","McCree","Mei","Mercy","Moira","Orisa","Pharah","Reaper","Reinhardt","Roadhog","Sigma","Soldier: 76","Sombra","Symmetra","Torbjörn","Tracer","Widowmaker","Winston","Wrecking Ball","Zarya","Zenyatta"};
     }
-    public ArrayList<String> values = new ArrayList<String>();
-    public ArrayList<String> heroConfirm = new ArrayList<String>();
+    public ArrayList<String> values = new ArrayList<String>();  //our results list
+    public ArrayList<String> heroConfirm = new ArrayList<String>(); //tools for backchecking data to make sure it's all there and skip unnecessary characters.
     public ArrayList<String> compConfirm = new ArrayList<String>();
     public String[] compRates;
     URL url;
-    public infoGrabber(URL newrl) {
+    public infoGrabber(URL newrl) { //the constructor. Kept separate from the rest for better timing of the execution in the main
         url = newrl;
     }
-    public void execute() throws IOException {
+    public void execute() throws IOException {  //our core function, this does most of the work here
         //use URL info generated in regular class to find a player page
         if(new File("idump.txt").delete()){System.out.println("Cleared.");} //a safe way to make sure this file gets overwritten rather than appended
-        values.clear();
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream("idump.txt");
+        values.clear(); //emptying this just in case someone decides to reuse the program. May be a little unstable though.
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());    //using nio to get our html file
+        FileOutputStream fos = new FileOutputStream("idump.txt");   //and dumping it to local
         FileChannel fc = fos.getChannel();
         fc.transferFrom(rbc, 0, Long.MAX_VALUE);
         fos.close();
         String line;
         Stream<String> lines = Files.lines(Paths.get("idump.txt"));
-        line = lines.skip(1522).findFirst().get();
+        line = lines.skip(1522).findFirst().get();  //So the first 1,522 lines are entirely junk. The actual .site tag that hold all of our data is all on line 1523
         lines.close();
         String nex = "";
 
-        nex = line;
-        Pattern pattern = Pattern.compile("(competitive-rank-level\">\\d{3,4})");
-        Matcher matcher = pattern.matcher(nex);
-        String[] srs = new String[3];
+        nex = line; //we'll be redoing this a couple times, but this now holds ALL of line 1523. Which is really big. I do this more for comfort than necessity
+        Pattern pattern = Pattern.compile("(competitive-rank-level\">\\d{3,4})"); //don't use regex for HTML, they said
+        Matcher matcher = pattern.matcher(nex); //but did I listen?
+        String[] srs = new String[3];   //no.
         int x=0;
-        while(matcher.find() && x<3) {
+        while(matcher.find() && x<3) {  //our first data: this is the player's competitive ranks. Hopefully they have some, because I feel this will break if they don't
             srs[x] = matcher.group(0);
             srs[x] = srs[x].replaceAll("competitive-rank-level\">", "");
             x++;
         }
-        pattern = Pattern.compile("(0x02E\\d*\\w*)\"\\soption-id=\"([a-zA-Z]*\\s?.?\\s?[a-zA-Z0-9]*)");
+        pattern = Pattern.compile("(0x02E\\d*\\w*)\"\\soption-id=\"([a-zA-Z]*\\s?.?\\s?[a-zA-Z0-9]*)"); //the worst regex of them all
         matcher = pattern.matcher(nex);
         int cycle = 0;
         int loca = 0;
-        while(matcher.find()) {
+        while(matcher.find()) { //this is the simpler of two regex issues
+            // this mess finds hero names in the list, and adds them to a list that I double check against the master list so that empty heroes don't mess with the rest of the data. Looking at you, Lucio.
             if(matcher.group(0).contains("ALL HEROES")) {
                 cycle++;
                 loca=0;
@@ -70,27 +71,27 @@ public class infoGrabber {
             loca++;
         }
         compRates = new String[] {srs[0],srs[1],srs[2]}; //[0] contains tank, [1] contains DPS, [2] contains support
-        pattern = Pattern.compile("([a-zA-Z]*(-*\\s*\\d*[a-zA-z]*)*</td><[^>]*>(\\d*:?\\.?%?\\d*:?\\d*))");
+        pattern = Pattern.compile("([a-zA-Z]*(-*\\s*\\d*[a-zA-z]*)*</td><[^>]*>(\\d*:?\\.?%?\\d*:?\\d*))"); //did I say worst earlier? this one's far worse.
         matcher = pattern.matcher(nex);
         String found = "";
         int z=0;
         boolean firstDamage = false; // a boolean that fixes Ana's stats appearing under All Heroes
         boolean ahnm = true; // a boolean that fixes the order of name addition and generation
         boolean c = false;
-        boolean luciohs = false;
+        boolean luciohs = false;        //and here we have three booleans that are entirely for checking when heroes don't exist or have missing data or all that jazz
         boolean onemore = false;
         //goal is to replace anything that is NOT (number/word number/word...number/word</td><...>number/decimal/time</td></tr>) with nothing
         while(matcher.find()) {
             found = matcher.group(0);
-            found = found.replace("</td><td class=\"DataTable-tableColumn\">", ": ");
-            found = found.replace("</td></tr>", "");
+            found = found.replace("</td><td class=\"DataTable-tableColumn\">", ": "); //cleanup
+            found = found.replace("</td></tr>", "");    //cleanup
             if(found.startsWith("rn Kills")) {
-                found =found.replace("rn Kills","Torbjörn Kills");
+                found =found.replace("rn Kills","Torbjörn Kills"); //have to replace the accent because I couldn't get regex to include this
             }
             if(found.startsWith("d:") || found.startsWith("d -")) {
-                found = found.replace("d", "Enemies EMP'd");
+                found = found.replace("d", "Enemies EMP'd");    //same with the apostrophe since it's just one case.
             }
-            if(found.contains("All Damage Done - Most in Game")) {
+            if(found.contains("All Damage Done - Most in Game")) {  //there were lots of shenanigans involving heroes on my example account not having enough playtime for the stats to show in the right order
                 if(ahnm) { 
                             ahnm = false;
                             values.add(heroList[z]);
@@ -112,8 +113,8 @@ public class infoGrabber {
                         luciohs = true;
                     }
                 }
-            }
-            if(found.contains("All Damage Done:")&& firstDamage&&!c &&!luciohs) {
+            }   //and all of that is just to correctly make sure that low play time heroes show up where they should.
+            if(found.contains("All Damage Done:")&& firstDamage&&!c &&!luciohs) {   // our first "normal" identifier
                 while(!heroConfirm.contains(heroList[z]) && z<34) {
                     values.add(heroList[z]);
                     z++;
@@ -124,7 +125,7 @@ public class infoGrabber {
                 onemore=false;
                 }
             }
-            if(found.contains("All Damage Done:") && firstDamage&&c&&!luciohs) {
+            if(found.contains("All Damage Done:") && firstDamage&&c&&!luciohs) { //its equivalent, but for comp. No, they aren't different. Yes, I'm too lazy to remove it.
                 while(!compConfirm.contains(heroList[z]) && z<32) {
                     values.add(heroList[z]);
                     z++;
@@ -135,22 +136,22 @@ public class infoGrabber {
                 z++;
                 }
             }
-            if(found.contains("All Damage Done:") && !firstDamage) {
+            if(found.contains("All Damage Done:") && !firstDamage) { //The BEST category always shows up before COMBAT on all heroes, this skips that occurrence.
                 firstDamage = true;
             }
-            if(luciohs && found.contains("All Damage Done:")) {
+            if(luciohs && found.contains("All Damage Done:")) { //basically the same case as the above, but the trigger is done differently.
                 luciohs = false;
             }
             if(z>=33){z=0;firstDamage=false;c=true;ahnm=true;onemore=false;}
-            if(found!="" && found!= " ") {
-                values.add(found);
+            if(found!="" && found!= " ") { //for whatever reason our regex finds empty lines between every important bit of data, very cool.
+                values.add(found);  //and casually do this over 3000 times or however many there are
             }
         }
-        System.out.println("Done.");
+        System.out.println("Done."); //hey maybe the user likes feedback. I know I do.
     }
         
 
-    public ArrayList<String> getReturns() {
+    public ArrayList<String> getReturns() { //four returns, in which we only use two.
         return values;
     }
     public String[] getComp() {
